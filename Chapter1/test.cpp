588fa77dc321543c565d66bbc8933ec5b0225ac2
oleksharh <oleksharh@gmail.com>
2024-07-31 22:50:15 +0100

    initial commit

---
+++ Chapter1/test.cpp
@@ -0,0 +1,7 @@
+// This program outputs the message "Hello, World!" to the monitor
+#include <iostream> // gain access to the C++ standard library
+
+int main() {
+    std::cout << "Hello, World!\n"; // output "Hello, World!"
+    return 0;
+}
