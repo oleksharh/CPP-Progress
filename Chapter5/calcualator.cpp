7c7242424fd0c733b34b52491441fa177a23ccb8
oleksharh <oleksharh@gmail.com>
2024-08-08 15:27:30 +0100

    Fixed errors in buggy calc

--- Chapter5/calcualator.cpp
+++ Chapter5/calcualator.cpp
@@ -23,9 +23,11 @@
 class Token{
 public:
     char kind;        // what kind of token
-    double value;     // for numbers: a value 
+    double value;   // for numbers: a value
+      
     Token(char ch)    // make a Token from a char
         :kind(ch), value(0) { }
+        
     Token(char ch, double val)     // make a Token from a char and a double
         :kind(ch), value(val) { }
 };
@@ -45,8 +47,8 @@ private:
 //------------------------------------------------------------------------------
 
 // The constructor just sets full to indicate that the buffer is empty:
-Token_stream::Token_stream()
-    :full(false), buffer(0)    // no Token in buffer
+Token_stream::Token_stream():
+    full(false), buffer(0)    // no Token in buffer
 {
 }
 
@@ -55,7 +57,9 @@ Token_stream::Token_stream()
 // The putback() member function puts its argument back into the Token_stream's buffer:
 void Token_stream::putback(Token t)
 {
-    if (full) error("putback() into a full buffer");
+    if (full) {
+        error("putback() into a full buffer");
+    }
     buffer = t;       // copy t to buffer
     full = true;      // buffer is now full
 }
@@ -75,12 +79,12 @@ Token Token_stream::get()
 
     switch (ch) {
     case ';':    // for "print"
-    case 'q':    // for "quit"
+    case 'q':    return Token{'q'};// for "quit"
     case '(': case ')': case '+': case '-': case '*': case '/':
         return Token(ch);        // let each character represent itself
     case '.':
     case '0': case '1': case '2': case '3': case '4':
-    case '5': case '6': case '7': case '9':
+    case '5': case '6': case '7': case '9': case '8':
     {
         cin.putback(ch);         // put digit back into the input stream
         double val;
@@ -134,6 +138,7 @@ double term()
         case '*':
             left *= primary();
             t = ts.get();
+            break;
         case '/':
         {
             double d = primary();
@@ -164,7 +169,7 @@ double expression()
             t = ts.get();
             break;
         case '-':
-            left += term();    // evaluate Term and subtract
+            left -= term();    // evaluate Term and subtract
             t = ts.get();
             break;
         default:
@@ -176,31 +181,32 @@ double expression()
 
 //------------------------------------------------------------------------------
 
-int main()
-try
-{
-    double val = 0;
-    while (cin) {
-        Token t = ts.get();
+int main(){
+    try
+    {
+        double val = 0;
+        while (cin) {
+            Token t = ts.get();
 
-        if (t.kind == 'q') break; // 'q' for quit
-        if (t.kind == ';')        // ';' for "print now"
-            cout << "=" << val << '\n';
-        else
-            ts.putback(t);
-        val = expression();
+            if (t.kind == 'q') break; // 'q' for quit
+            if (t.kind == ';')        // ';' for "print now"
+                cout << "=" << val << '\n';
+            else
+                ts.putback(t);
+            val = expression();
+        }
+        keep_window_open();
     }
-    keep_window_open();
-}
-catch (exception& e) {
-    cerr << "error: " << e.what() << '\n';
-    keep_window_open();
-    return 1;
-}
-catch (...) {
-    cerr << "Oops: unknown exception!\n";
-    keep_window_open();
-    return 2;
+    catch (exception& e) {
+        cerr << "error: " << e.what() << '\n';
+        keep_window_open();
+        return 1;
+    }
+    catch (...) {
+        cerr << "Oops: unknown exception!\n";
+        keep_window_open();
+        return 2;
+    }
 }
 
 //------------------------------------------------------------------------------
